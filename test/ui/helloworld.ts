01be186f3cfb4dad9118e718c2092fc0768bfd2b
David Mitchell <monch1962@gmail.com>
2018-12-13 09:42:19 +1100

    Moved UI tests to Testcafe

---
+++ test/ui/helloworld.ts
@@ -0,0 +1,9 @@
+import { Selector } from 'testcafe';
+
+fixture `Homepage`
+    .page `http://localhost:8080/`;
+
+test('Test helloworld content on homepage', async t => {
+    await t
+        .expect(Selector('html').innerText).contains('hello world!')
+});
