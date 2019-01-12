9cc49e42c1e90f02ff5101096c2267d0ac235768
David Mitchell <monch1962@gmail.com>
2019-01-12 23:02:44 +1100

    Adding integration test coverage

--- test/integration/test.js
+++ test/integration/test.js
@@ -1,11 +1,12 @@
 const chai = require('chai')
 const expect = chai.expect
+const assert = chai.assert
 // const describe = chai.describe
 // const it = chai.it
 const chaiHttp = require('chai-http')
 chai.use(chaiHttp)
 
-describe('trumpwall', () => {
+describe('trumpwall', async () => {
   it('it should return "nothing to see here"', (done) => {
     chai.request('http://localhost:8080')
       .get('/trumpwall')
@@ -13,7 +14,42 @@ describe('trumpwall', () => {
         expect(err).to.be.null
         expect(res).to.have.status(200)
         expect(res.text).to.equal('nothing to see here')
-        done()
+        // done()
       })
+    done()
   })
 })
+/*
+chai.use(require('chai-as-promised'))
+
+describe('trumpwall (promises)', async () => {
+  it('it should return "nothing to see here"2', (done) => {
+    chai.request('http://localhost:8080')
+      .get('/trumpwall')
+      .end((err, res) => {
+        return expect(new Promise((resolve, reject) => {
+          expect(err).to.be.null
+          expect(res).to.have.status(200)
+          expect(res.text).to.equal('nothing to see here')
+        }))
+      })
+      .catch((error) => {
+        assert.isNotOk(error, 'Promise error')
+      })
+  })
+
+  it('it should return "nothing to see here"3', (done) => {
+    return new Promise((resolve, reject) => {
+      chai.request('http://localhost:8080')
+        .get('/trumpwall')
+        .end((err, res) => {
+          expect(err).to.be.null
+          expect(res).to.have.status(200)
+          expect(res.text).to.equal('nothing to see here')
+        })
+    })
+      .catch((error) => {
+        assert.isNotOk(error, 'Promise error')
+      })
+  })
+}) */
