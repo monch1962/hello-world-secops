1bd558b0cfe7fcaeee93161c910f47ee62a9a1a6
David Mitchell <monch1962@gmail.com>
2019-01-12 20:26:19 +1100

    Adding integration test coverage

--- test/integration/test.js
+++ test/integration/test.js
@@ -1,9 +1,9 @@
 const expect = require('chai').expect
 const code = require('../../components/trumpwall.js')
 
-describe('trumpwall', () => {
-  it("should return 'nothing to see here'", () => {
-    const actual = code.content()
+describe('trumpwall', async () => {
+  it("should return 'nothing to see here'", async () => {
+    const actual = await code.content()
     expect(actual).to.equal('nothing to see here')
   })
 })
